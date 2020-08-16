780f1bb06b88088658ca81f2674d9d243a7edbec
hoodielive <hoodielive@gmail.com>
2020-08-15 22:02:33 -0400

    Okay, the error was from (+ (square x) (square y))

--- SICP/Chapter_1/formal_parameters.rkt
+++ SICP/Chapter_1/formal_parameters.rkt
@@ -1,13 +1,19 @@
 #lang scheme
 
+(define (square x ) (* x x))
+
 (square 21)
 (square (+ 2 5))
 (square (square 3))
+(square (square 3))
 
-(+ (square x) (square y))
+; define a procedure sum-of-squares
+; will take 2 args and poroduce a square
 
 (define (sum-of-squares x y)
-  (+ (square x) (square y)))
-
-; Okay run it.
+	(+ (square x) (square y)))
 (sum-of-squares 3 4)
+
+(define (f a)
+	(sum-of-squares (+ a 1) (* 2)))
+(f 5)
