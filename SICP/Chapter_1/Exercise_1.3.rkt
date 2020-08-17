88eb51cd3f9c0776c026354131d91f32bd1885e9
hoodielive <hoodielive@gmail.com>
2020-08-16 21:43:06 -0400

    Exercise 1.3

---
+++ SICP/Chapter_1/Exercise_1.3.rkt
@@ -0,0 +1,10 @@
+#lang scheme
+
+(define (square x) (* x x))
+(define (sum-of-squares x y)
+    (+ (square x) (square y)))
+
+(define (sum-of-squares-top-two a b c)
+    (cond ((and (< a b) (< a c)) (sum-of-squares b c))
+          ((and (< b a) (< b c)) (sum-of-squares a c))
+          (else (sum-of-squares a b))))
