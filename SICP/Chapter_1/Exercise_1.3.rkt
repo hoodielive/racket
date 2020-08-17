434362e70a22564b57233b0c1c80d16ffcad0c68
hoodielive <hoodielive@gmail.com>
2020-08-16 22:46:53 -0400

    Beautiful Functions.. lol

--- SICP/Chapter_1/Exercise_1.3.rkt
+++ SICP/Chapter_1/Exercise_1.3.rkt
@@ -6,9 +6,10 @@
 (define (sum-of-squares x y)
     (+ (square x) (square y)))
 
+; beautiful!
 (define (sum-of-squares-top-two a b c)
     (cond ((and (< a b) (< a c)) (sum-of-squares b c))
           ((and (< b a) (< b c)) (sum-of-squares a c))
           (else (sum-of-squares a b))))
 
-; um.. 
+
